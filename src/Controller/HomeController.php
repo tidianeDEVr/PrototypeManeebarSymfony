24aebec611fc9afe31b3bdc838b92f2c4e06cd45
Cheikh Tidiane Ndiaye <cheikhtiindiaye@gmail.com>
2022-07-07 15:28:51 +0200

    Premier Commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/nos-services', name: 'nos_services')]
+    public function showServices(): Response
+    {
+        return $this->render('home/nos.services.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/qui-sommes-nous', name: 'qui_sommes_nous')]
+    public function showWhoAreUs(): Response
+    {
+        return $this->render('home/qui.sommes.nous.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/contacts', name: 'contacts')]
+    public function showContacts(): Response
+    {
+        return $this->render('home/contacts.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
